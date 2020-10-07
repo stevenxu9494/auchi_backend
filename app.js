780f69667381ff2b088280005758b212199c035a
stevenxu9494 <stevenxu9494@gmail.com>
2020-10-07 16:57:15 +1100

    production setup

--- app.js
+++ app.js
@@ -6,19 +6,7 @@ const config = require('./config')
 
 const cors = require('koa2-cors')
 // 具体参数我们在后面进行解释
-app.use(cors({
-  origin: function (ctx) {
-      if (ctx.url === '/xc') {
-          return "*"; // 允许来自所有域名请求
-      }
-      return '0.0.0.0:8080'; // 这样就能只允许 http://101.190.45.246:8080 这个域名的请求了
-  },
-  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-  maxAge: 5,
-  credentials: true,
-  allowMethods: ['GET', 'POST', 'DELETE'],
-  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
-}))
+app.use(cors())
 
 
 // 解析请求体
