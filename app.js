69a77300f671fbddf5b64143d5faedddff2d90eb
stevenxu9494 <stevenxu9494@gmail.com>
2020-10-11 12:49:47 +1100

    setup

--- app.js
+++ app.js
@@ -7,22 +7,22 @@ const config = require('./config')
 // cors 解决跨域问题
 const cors = require('koa2-cors')
 // 1. 阿里云不需要加请求头
-app.use(cors())
+// app.use(cors())
 // 2. 本地访问需要加请求头
-// app.use(cors({
-//   origin: function (ctx) {
-//       if (ctx.url === '/test') {
-//           return "*"; // 允许来自所有域名请求
-//       }
-//       return 'http://localhost:8080'; 
-//   },
-//   exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-//   maxAge: 5,
-//   credentials: true,
-//   allowMethods: ['GET', 'POST', 'DELETE'],
-//   allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
-// }))
-// 解析请求体
+app.use(cors({
+  origin: function (ctx) {
+      if (ctx.url === '/xc') {
+          return "*"; // 允许来自所有域名请求
+      }
+      return 'http://localhost:8080'; 
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
+解析请求体
 app.use(bodyParser())
 
 const router = require('./routes')
