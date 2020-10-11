51cecc5cf3050de51b6d3132ae9821d8361f9608
stevenxu9494 <stevenxu9494@gmail.com>
2020-10-11 12:25:53 +1100

    Change README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # auchi_backend
+## 简单的步骤
+1. ```git pull``` 拉取
+2. ```git commit + push``` 更新仓库
--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const app = new Koa()
 const bodyParser = require('koa-bodyparser')
 const config = require('./config')
 
+// cors 解决跨域问题
 const cors = require('koa2-cors')
-// 具体参数我们在后面进行解释
+// 1. 阿里云不需要加请求头
 app.use(cors())
-
-
+// 2. 本地访问需要加请求头
+// app.use(cors({
+//   origin: function (ctx) {
+//       if (ctx.url === '/test') {
+//           return "*"; // 允许来自所有域名请求
+//       }
+//       return 'http://localhost:8080'; 
+//   },
+//   exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+//   maxAge: 5,
+//   credentials: true,
+//   allowMethods: ['GET', 'POST', 'DELETE'],
+//   allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+// }))
 // 解析请求体
 app.use(bodyParser())
 
